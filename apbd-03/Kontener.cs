bf3fc68ed81f2ab076033be3039860b7afdc57b6
Mikomaks <ytmikos@gmail.com>
2025-03-19 19:44:05 +0100

    Dodanie plików

---
+++ apbd-03/Kontener.cs
@@ -0,0 +1,52 @@
+namespace apbd_03;
+
+public abstract class Kontener
+{
+    /*
+     * Masę ładunku (w kilogramach)
+       Wysokość (w centymetrach)
+       Waga w własna (waga samego kontenera, w kilogramach)
+       Głębokość (w centymetrach)
+       Numer seryjny
+       Format numeru to KON-C-1
+       Pierwszy człon numery to zawsze "KON"
+       Drugi człon reprezentuje rodzaj kontenera
+       Trzeci człon to liczba. Liczby powinny być unikalne. Nie powinno być możliwości powstania dwóch kontenerów o tym
+       samym numerze. Numery powinny być generowane przez system.
+       Maksymalna ładowność danego kontenera w kilogramach
+     */
+
+    protected double masa;
+    protected double height;
+    protected double own_masa;
+    protected double depth;
+    protected string serial;
+    protected double max;
+    
+    private static int serializer = 1;
+    
+
+    private string typ = "00";
+
+    protected Kontener(double height, double ownMasa, double depth, double max)
+    {
+        this.masa = masa;
+        this.height = height;
+        this.own_masa = ownMasa;
+        this.depth = depth;
+        this.max = max;
+
+        this.serial = "KON-" + typ + "-" +
+                      (serializer.ToString().Length < 10 ? "0" + serializer.ToString() : serializer.ToString());
+    }
+
+    protected static int getNextSerial()
+    {
+        return serializer++;
+    }
+
+    public abstract string empty();
+    
+    public abstract string fill(double how_much);
+
+}
---
+++ apbd-03/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using apbd_03;
+
+Dictionary<string, double> produkty = new Dictionary<string, double>
+{
+    {"Bananas",13.3},
+    {"Chocolate",18},
+    {"Fish",2},
+    {"Meat",-15},
+    {"Ice cream",-18},
+    {"Frozen pizza",-30},
+    {"Cheese",7.2},
+    {"Sausages",5},
+    {"Butter",20.5},
+    {"Eggs",19}
+};
+
+Kontener_Chlodniczy test1 = new Kontener_Chlodniczy(1000,10,300,2000,
+    produkty.ElementAt(^1).Key,produkty.ElementAt(^1).Value);
+Console.WriteLine(test1);
+
+Kontener_Cieczowy test2 = new Kontener_Cieczowy(1000,10,300,200,true);
+Console.WriteLine(test2);
+
+/*List<Kontener> kontenery = new List<Kontener>{test1,test2};
+
+for (int i = 0; i < 10; i++)
+{
+    kontenery.Add(new Kontener_Chlodniczy(1,1,1,1,4));
+}
+
+Console.WriteLine(kontenery[^1]);*/
+
+Console.WriteLine(test1.fill(100,produkty.ElementAt(^1).Key));
